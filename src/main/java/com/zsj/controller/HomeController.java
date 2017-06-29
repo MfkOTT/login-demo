0de73fac33cf097eeeee239cbe25a9639a94a31e
MfkOTT <382112422@qq.com>
2017-06-29 13:42:06 +0800

    update config

--- src/main/java/com/zsj/controller/HomeController.java
+++ src/main/java/com/zsj/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.zsj.controller;
 
 import com.zsj.bean.Msg;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -11,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class HomeController {
+    private static Logger logger = LoggerFactory.getLogger(HomeController.class);
     @RequestMapping("/")
     public String index(Model model) {
+        logger.info("------login in-----");
         Msg msg = new Msg("测试标题", "测试内容", "额外信息，只对管理员显示");
         model.addAttribute("msg", msg);
         return "index";
